d5ef447ea8b8e829b8df60906d98be9f84a44070
sardor <ruziqulovsardor3@gmail.com>
2022-08-01 13:31:31 +0500

    code

--- bin/if01.dart
+++ bin/if01.dart
@@ -8,5 +8,21 @@ Args:
 Returns:
     int: return answer.
 */
+func(int a, int b, int c){
+    if(a > b){
+    if(a > c){
+        return a;
+    }
+    }
+   if(b > a){
+    if(b > c){
+        return b;
+    }
+   }
+   return c;
+}
+
+
 void main() {
+    print(func(21, 13, 11));
 }
