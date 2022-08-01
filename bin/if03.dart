d3a31232fd1eb50e93f4b7d608c86b6a67ca9c4a
sardor <ruziqulovsardor3@gmail.com>
2022-08-01 13:54:24 +0500

    code

--- bin/if03.dart
+++ bin/if03.dart
@@ -7,5 +7,20 @@ Args:
     c: Third number.
 Returns:
     int: return answer. */
+func(int a, int b, int c){
+    if(a > b){
+    if(a < c){
+    return a;
+    }
+    }
+    if(b > a){
+    if(b < c){
+    return b;
+    }
+    }
+    return c;
+}
+
 void main() {
+    print(func(21, 4, 45 ));
 }
